7368a39e16fd1fe5797183216cec6679b4ea36c5
tomoya0x03a <yamada0x03a@gmail.com>
2024-06-11 16:50:35 +0900

    storybookにリセットCSSを設定した。

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,6 +1,25 @@
 import type { Preview } from "@storybook/react";
-import { ThemeProvider } from "styled-components";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
 import { theme } from "../src/themes";
+
+export const GlobalStyle = createGlobalStyle`
+  html,
+  body,
+  textarea {
+    padding: 0;
+    margin: 0;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+  * {
+    box-sizing: border-box;
+  }
+  a {
+    text-decoration: none;
+    transition: .25s;
+    color: #000000;
+  }
+`;
 
 const preview: Preview = {
   parameters: {
@@ -14,7 +33,7 @@ const preview: Preview = {
   decorators: [
     (Story, context) => {
       return (
-        <ThemeProvider thme={theme}>
+        <ThemeProvider theme={theme}>
           <GlobalStyle />
           <Story />
         </ThemeProvider>
